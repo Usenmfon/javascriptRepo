9213544c4152084c420da2935d6296aea294e867
Usenmfon <usenmfonuko@gmail.com>
2021-06-14 13:41:04 +0100

    worked with number objects

---
+++ myExercise/number-object.js
@@ -0,0 +1,6 @@
+var originalNumber = 10.23456;
+console.log("Using toFixed: " + originalNumber.toFixed(3));
+console.log("Using toPrecision: " + originalNumber.toPrecision(3));
+
+var randomNumber = Math.floor((Math.random() * 10) + 1);
+console.log(randomNumber);
