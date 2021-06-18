1fdac81d79f9ca9b0e5e3ec8a98290e406578437
Usenmfon <usenmfonuko@gmail.com>
2021-06-18 11:43:11 +0100

    new exercise

--- myExercise/home-page.js
+++ myExercise/home-page.js
@@ -1,4 +1,21 @@
-var greetings = "Hello there, Good morning";
+var tag = document.createElement('p');
+var text = document.createTextNode("This is a new element and text added");
+tag.appendChild(text);
 
-var elGreetings = document.getElementById('hello');
-elGreetings.textContent = greetings;
+var element = document.getElementById('hello');
+element.appendChild(tag);
+
+var fruitArray = ['Mango', 'Apple', 'Bananna'];
+
+for (var i = 0; i < fruitArray.length; i++) {
+    var newTag = document.createElement('p');
+    var inputText = document.createTextNode(fruitArray[i]);
+    newTag.appendChild(inputText);
+
+    var element = document.getElementById('hello');
+
+    console.log(element.appendChild(newTag));
+}
+
+var changeCss = document.querySelector("#hello");
+changeCss.style.backgroundColor = "red";
