683d5ba8036c430d3db1b51875a432e89a68324b
Usenmfon <usenmfonuko@gmail.com>
2021-06-28 13:24:48 +0100

    truncate a string

---
+++ truncateString.js
@@ -0,0 +1,29 @@
+// const truncate = (str, num) => {
+//     if (str.length > num) {
+//         var char = str.charAt(num)
+
+//         var newString = str.replace(char, '...')
+//         return newString.split('w').pop();
+
+//     }
+// }
+
+// console.log(truncate('Hello there, How are you doing?', 15))
+
+// // console.log('hello'.lastIndexOf(2));
+// // console.log('hello'.charAt(2));
+// // console.log('Hello there, How are you doing?'.charAt(15))
+// // console.log('hello'.replace('e', 'w'))
+// var str = 'Hello'
+// console.log(str.split('e').pop())
+// console.log(str)
+
+function findElement(arr, func) {
+    let num = 0;
+    for (var i = 0; i < arr.length; i++) {
+
+    }
+    return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
