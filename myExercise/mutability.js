0e9e513184284c0ccc7632942550cbcd3cfbbb0b
Usenmfon <usenmfonuko@gmail.com>
2021-07-07 15:24:01 +0100

    added a bootstrap file

---
+++ myExercise/mutability.js
@@ -0,0 +1,32 @@
+// const hummus = function(factor) {
+//     const ingredient = function(amount, unit, name) {
+//         let ingredientAmount = amount * factor;
+//         if (ingredientAmount > 1) {
+//             unit += "s";
+//         }
+//         console.log(`${ingredientAmount} ${unit} ${name}`);
+//     };
+//     ingredient(1, "can", "chickpeas");
+//     ingredient(0.25, "cup", "tahini");
+//     ingredient(0.25, "cup", "lemon juice");
+//     ingredient(1, "clove", "garlic");
+//     ingredient(2, "tablespoon", "olive oil");
+//     ingredient(0.5, "teaspoon", "cumin");
+// };
+
+// hummus(2)
+
+console.log("The future says: " + future());
+
+function future() {
+    return "It will be amazing";
+}
+
+console.log("2", "gt", 4);
+
+function multiplier(factor) {
+    return number => number * factor;
+}
+
+let twice = multiplier(3);
+console.log(twice(2))
