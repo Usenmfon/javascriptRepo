23a87a8844b6009c45d7fcd89db3635f05d205e5
Usenmfon <usenmfonuko@gmail.com>
2021-07-17 13:26:24 +0100

    dom manipulation

---
+++ myExercise/dom.js
@@ -0,0 +1,17 @@
+const replaceImages = function() {
+    let images = document.getElementsByTagName('img')
+    for (let i = images.length - 1; i >= 0; i--) {
+        let image = images[i];
+        if (image.alt) {
+            let text = document.createTextNode(image.alt)
+            image.parentNode.replaceChild(text, image);
+        }
+    }
+
+    let paras = document.getElementsByTagName('p');
+    for (let para of Array.from(paras)) {
+        if (para.getAttribute("data-classified") == 'secret') {
+            para.remove();
+        }
+    }
+}
